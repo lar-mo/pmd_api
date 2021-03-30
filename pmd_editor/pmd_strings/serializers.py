ba1215cd5343177fa610d3fd626800ffffa4f49e
Larry Moiola <lmoiola@gmail.com>
2021-03-30 11:42:27 -0700

    adding class for v3; adding alias for string_en > string

--- pmd_editor/pmd_strings/serializers.py
+++ pmd_editor/pmd_strings/serializers.py
@@ -3,11 +3,18 @@ from rest_framework import serializers
 from .models import String
 
 class StringSerializerv1(serializers.ModelSerializer):
+    string = serializers.CharField(source='string_en') # for backwards compatibility
     class Meta:
         model = String
         fields = ['id', 'location', 'updated', 'string']
 
 class StringSerializerv2(serializers.ModelSerializer):
+    string = serializers.CharField(source='string_en') # for backwards compatibility
     class Meta:
         model = String
         fields = ['location', 'string']
+
+class StringSerializerv3(serializers.ModelSerializer):
+    class Meta:
+        model = String
+        fields = ['location', 'string_en', 'string_es', 'string_fr', 'string_de']
